a6cd1c6c41ac74de9f66ede82b085c80162e9c3b
ShamimMolla960 <shamimMolla960>
2023-05-31 23:55:06 +0600

    all file and folder

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    "use strict"
+    $(".movie_slaider").slick({
+        arrows:false,
+        centerMode:true,
+        infinite:true,
+        slidesToShow:5,
+        slidesToScroll:1,
+        centerPadding:"100px",
+        focusOnSelect:true,
+
+    })
+})
